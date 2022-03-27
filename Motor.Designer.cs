f4c2a01beb0052b809ea9052d3cf95a607a7e90c
Hwang <hwanghyeongyong@gmail.com>
2022-03-27 23:12:01 +0100

    스레드 이용한 시리얼포트 라인 추가

---
+++ Motor.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace Motor
+{
+    partial class Motor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbConnect = new System.Windows.Forms.GroupBox();
+            this.txtBaudrate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPortname = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtRead = new System.Windows.Forms.RichTextBox();
+            this.txtWrite = new System.Windows.Forms.TextBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.serialPortIn = new System.IO.Ports.SerialPort(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
+            this.gbConnect.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbConnect
+            // 
+            this.gbConnect.Controls.Add(this.txtBaudrate);
+            this.gbConnect.Controls.Add(this.label2);
+            this.gbConnect.Controls.Add(this.txtPortname);
+            this.gbConnect.Controls.Add(this.label1);
+            this.gbConnect.Location = new System.Drawing.Point(39, 35);
+            this.gbConnect.Name = "gbConnect";
+            this.gbConnect.Size = new System.Drawing.Size(280, 141);
+            this.gbConnect.TabIndex = 0;
+            this.gbConnect.TabStop = false;
+            this.gbConnect.Text = "Connection";
+            // 
+            // txtBaudrate
+            // 
+            this.txtBaudrate.FormattingEnabled = true;
+            this.txtBaudrate.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "14400",
+            "57600",
+            "115200"});
+            this.txtBaudrate.Location = new System.Drawing.Point(118, 97);
+            this.txtBaudrate.Name = "txtBaudrate";
+            this.txtBaudrate.Size = new System.Drawing.Size(121, 23);
+            this.txtBaudrate.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Baud Rate :";
+            // 
+            // txtPortname
+            // 
+            this.txtPortname.FormattingEnabled = true;
+            this.txtPortname.Location = new System.Drawing.Point(118, 51);
+            this.txtPortname.Name = "txtPortname";
+            this.txtPortname.Size = new System.Drawing.Size(121, 23);
+            this.txtPortname.TabIndex = 1;
+            this.txtPortname.DropDown += new System.EventHandler(this.txtPortname_DropDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port Name :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSend);
+            this.groupBox1.Controls.Add(this.txtRead);
+            this.groupBox1.Controls.Add(this.txtWrite);
+            this.groupBox1.Location = new System.Drawing.Point(325, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(463, 342);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial Monitor";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(365, 42);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtRead
+            // 
+            this.txtRead.Location = new System.Drawing.Point(16, 84);
+            this.txtRead.Name = "txtRead";
+            this.txtRead.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtRead.Size = new System.Drawing.Size(424, 236);
+            this.txtRead.TabIndex = 1;
+            this.txtRead.Text = "";
+            // 
+            // txtWrite
+            // 
+            this.txtWrite.Location = new System.Drawing.Point(16, 43);
+            this.txtWrite.Name = "txtWrite";
+            this.txtWrite.Size = new System.Drawing.Size(343, 25);
+            this.txtWrite.TabIndex = 0;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(116, 206);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(94, 23);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(216, 206);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(116, 246);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(94, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(216, 246);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(94, 23);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(341, 373);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Motor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.gbConnect);
+            this.Controls.Add(this.btnEdit);
+            this.Name = "Motor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Motor_Load);
+            this.gbConnect.ResumeLayout(false);
+            this.gbConnect.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.ComboBox txtBaudrate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox txtPortname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.RichTextBox txtRead;
+        private System.Windows.Forms.TextBox txtWrite;
+        private System.IO.Ports.SerialPort serialPortIn;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Motor.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Motor
+{
+    public partial class Motor : Form
+    {
+        public Thread serialThread;
+        public string rx_data;
+
+
+
+        public Motor()
+        {
+            InitializeComponent();
+        }
+
+        private void Motor_Load(object sender, EventArgs e)
+        {
+            this.Text = "Read serial Data using thread";
+            LoadConfigurationSetting();
+            gbConnect.Enabled = false;
+            btnSave.Enabled = false;
+            btnDisconnect.Enabled = false;
+            btnSend.Enabled = false;
+
+        }
+        private void LoadConfigurationSetting()
+        {
+            txtPortname.Text = ConfigurationManager.AppSettings["comname"];
+            txtBaudrate.Text = ConfigurationManager.AppSettings["combaudrate"];
+        }
+
+
+        private void SerialThread()
+        {
+            try
+            {
+                serialThread = new Thread(SerialMethod);
+                serialThread.Start();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Read Serial thread." + exc.Message);
+
+            }
+        }
+
+        private void SerialMethod()
+        {
+            while (serialPortIn.IsOpen) //SerialThread 객체에서 SerialMethod 메소드를 사용하여 계속 데이터를 취득함.
+            {
+                rx_data = serialPortIn.ReadExisting();
+                showRxData(rx_data);
+                Thread.Sleep(500);
+            }
+        }
+
+
+        public delegate void ShowSerialDataDelegate(string r);
+        private void showRxData(string rx_data) //Serial 스레드에서 UI 스레드 접근 시, Delegate로 접근
+        {
+            if (txtRead.InvokeRequired)
+            {
+                ShowSerialDataDelegate SSDD = showRxData;
+                Invoke(SSDD, rx_data);
+            }//기다림
+            else
+            {//기다림 후 실행
+                txtRead.AppendText(Environment.NewLine + rx_data);
+                txtRead.ScrollToCaret();
+            }
+
+        }
+
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (serialPortIn.IsOpen)
+            {
+                serialPortIn.WriteLine(txtWrite.Text);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            gbConnect.Enabled = true;
+            btnSave.Enabled = true;
+            btnEdit.Enabled = false;
+            btnConnect.Enabled = false;
+        }
+
+        private void txtPortname_DropDown(object sender, EventArgs e)
+        {
+            txtPortname.Items.Clear();
+            string[] COMs = SerialPort.GetPortNames();
+                foreach (var item in COMs)
+                {
+                    txtPortname.Items.Add(item);
+                }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            serialPortIn.PortName = txtPortname.Text;
+            serialPortIn.BaudRate = int.Parse(txtBaudrate.Text);
+            serialPortIn.DataBits = 8;
+            serialPortIn.StopBits = StopBits.One;
+            serialPortIn.Parity = Parity.None;
+
+            serialPortIn.Open();
+            if (serialPortIn.IsOpen)
+            {
+                SerialThread();
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+                btnEdit.Enabled = false;
+                btnSave.Enabled = false;
+                btnSend.Enabled = true;
+            }
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                config.AppSettings.Settings.Remove("comname");
+                config.AppSettings.Settings.Remove("combaudrate");
+
+                config.AppSettings.Settings.Add("comname", txtPortname.Text);
+                config.AppSettings.Settings.Add("combaudrate", txtBaudrate.Text);
+
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSettings");
+
+                btnConnect.Enabled = true;
+                btnEdit.Enabled = true;
+                btnSave.Enabled = false;
+                gbConnect.Enabled = false;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("saving Error," + exc.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtRead.Text = "";
+
+        }
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPortIn.IsOpen)
+                {
+                    serialPortIn.Close();
+                    btnDisconnect.Enabled = false;
+                    btnConnect.Enabled = true;
+                    btnEdit.Enabled = true;
+                    btnSave.Enabled = true;
+                    btnSend.Enabled = false;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Disconected error." + exc.Message);
+                
+            }
+        }
+       
+    }
+}
